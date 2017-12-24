5c096541df60155e5e2c406e551ee30ed2ebfa95
zhangxingshi@aliyun.com <root@Client130.zxs.com>
2017-12-24 17:54:09 +0800

    docker上运行discuz论坛

--- data/nginx/config_global.php
+++ data/nginx/config_global.php
@@ -32,7 +32,7 @@ $_config['db'][1]['dbuser']  		= 'forumuser';
 $_config['db'][1]['dbpw'] 	 	= '123456';
 $_config['db'][1]['dbcharset'] 		= 'utf8';
 $_config['db'][1]['pconnect'] 		= 0;
-$_config['db'][1]['dbname']  		= 'ultrax';
+$_config['db'][1]['dbname']  		= 'forum';
 $_config['db'][1]['tablepre'] 		= 'pre_';
 
 /**
